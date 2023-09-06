6ead9c7e9bab15d9fe9882bf7e827b4b88a17f1d
Ironspidr <janatanielroshan@gmail.com>
2023-09-06 15:39:53 -0700

    pages to html

---
+++ new_pages/assets/js/Ultimate-Sidebar-Menu.js
@@ -0,0 +1,16 @@
+jQuery(document).ready(function() {
+ 
+    $('.dismiss, .overlay').on('click', function() {
+        $('.sidebar').removeClass('active');
+        $('.overlay').removeClass('active');
+    });
+ 
+    $('.open-menu').on('click', function(e) {
+        e.preventDefault();
+        $('.sidebar').addClass('active');
+        $('.overlay').addClass('active');
+        // close opened sub-menus
+        $('.collapse.show').toggleClass('show');
+        $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+    });
+});
